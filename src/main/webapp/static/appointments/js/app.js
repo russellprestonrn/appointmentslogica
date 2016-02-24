21cd486ceb05aaae5b994533f0f599490d200537
amy6 <amy@motibox.com>
2016-02-24 12:20:07 -0500

    Remove ui.router and cleaned up a bit.

--- src/main/webapp/static/appointments/js/app.js
+++ src/main/webapp/static/appointments/js/app.js
@@ -1,21 +1,5 @@
 'use strict';
 
-angular.module('appointmentsApp', ['ui.router', 'ngAnimate', 'ui.bootstrap', 'daypilot', 'appointmentsApp.filters', 'appointmentsApp.services', 'appointmentsApp.controllers', 'appointmentsApp.directives'], function($stateProvider, $urlRouterProvider){
-
-    $urlRouterProvider.otherwise('/patient-view');
-
-    $stateProvider
-
-        .state('patient-view', {
-            url: '/patient-view',
-            templateUrl: 'js/templates/patientView.html',
-            authenticate: true
-        })
-
-        .state('practitioner-view', {
-            url: '/practitioner-view',
-            templateUrl: 'js/templates/practitionerView.html',
-            authenticate: true
-        })
-
+angular.module('appointmentsApp', ['ui.bootstrap', 'daypilot', 'appointmentsApp.filters', 'appointmentsApp.services',
+    'appointmentsApp.controllers', 'appointmentsApp.directives'], function(){
 });
--- src/main/webapp/static/appointments/js/controllers.js
+++ src/main/webapp/static/appointments/js/controllers.js
@@ -1,107 +1,13 @@
 'use strict';
 
 angular.module('appointmentsApp.controllers', []).controller('mainController',
-    ['$scope', '$rootScope',"$fhirApiServices", "$state",
-    function ($scope, $rootScope, $fhirApiServices, $state ) {
+    ['$scope', '$rootScope',"$fhirApiServices",
+    function ($scope, $rootScope, $fhirApiServices ) {
 
         $scope.user = "";
         $scope.patient = "";
         $scope.profile = "";
         $scope.appointments = [];
-
-
-        /**
-         *
-         *      FHIR SERVICE OAUTH2 AUTHENTICATION & APP INITIALIZATION
-         *
-         **/
-        $fhirApiServices.initClient().then(function(){
-            var query = {};
-            switch($fhirApiServices.getIntent()) {
-                case 'patient-view':
-                    $fhirApiServices.queryPatient()
-                        .done(function(patientResult){
-                            $scope.patient = patientResult;
-                            query.patient = $scope.patient.id;
-                            $scope.user = $scope.patient;
-                            $fhirApiServices.queryResourceInstances("Appointment", query)
-                                .done(function(resourceResults){
-                                    angular.forEach(resourceResults, function (resource) {
-                                        $scope.appointments.push(resource);
-
-                                    });
-                                    $rootScope.$emit('build-calendar');
-                                    $rootScope.$digest();
-                                });
-                        });
-                    $state.go('patient-view', {});
-                    break;
-                case 'practitioner-view':
-                    $fhirApiServices.getFhirProfileUser()
-                        .done(function(profileResult){
-                            $scope.profile = profileResult;
-                            query.practitioner = $scope.profile.id;
-                            $scope.user = $scope.profile;
-                            $fhirApiServices.queryResourceInstances("Appointment", query)
-                                .done(function(resourceResults){
-                                    angular.forEach(resourceResults, function (resource) {
-                                        $scope.appointments.push(resource);
-
-                                    });
-                                    $rootScope.$emit('build-calendar');
-                                    $rootScope.$digest();
-                                });
-                        });
-                    $state.go('practitioner-view', {});
-                    break;
-                default:
-                    if ($fhirApiServices.hasPatientContext()) {
-                       $fhirApiServices.queryPatient()
-                        .done(function(patientResult){
-                            $scope.patient = patientResult;
-                            query.patient = $scope.patient.id;
-                               $scope.user = $scope.patient;
-                               $fhirApiServices.queryResourceInstances("Appointment", query)
-                                .done(function(resourceResults){
-                                    angular.forEach(resourceResults, function (resource) {
-                                        $scope.appointments.push(resource);
-
-                                    });
-                                    $rootScope.$emit('build-calendar');
-                                    $rootScope.$digest();
-                                });
-                        }).fail(function(){
-                        });
-                        $state.go('patient-view', {});
-                    } else {
-                        $fhirApiServices.getFhirProfileUser()
-                            .done(function(profileResult){
-                                $scope.profile = profileResult;
-                                query.practitioner = $scope.profile.id;
-                                $scope.user = $scope.profile;
-                                $fhirApiServices.queryResourceInstances("Appointment", query)
-                                    .done(function(resourceResults){
-                                        angular.forEach(resourceResults, function (resource) {
-                                            $scope.appointments.push(resource);
-                                        });
-                                        $rootScope.$emit('build-calendar');
-                                        $rootScope.$digest();
-                                    });
-                            });
-                        $state.go('practitioner-view', {});
-                    }
-            }
-
-        });
-
-    }]).controller("PatientViewController",
-    function($scope){
-
-    }).controller("PractitionerViewController",
-    function($scope){
-
-    }).controller('AppointmentsCtrl', function($scope, $rootScope) {
-
         $scope.events = [];
 
         $scope.navigatorConfig = {
@@ -135,7 +41,7 @@ angular.module('appointmentsApp.controllers', []).controller('mainController',
             $scope.weekConfig.visible = true;
         };
 
-        $rootScope.$on('build-calendar', function(){
+        function buildCalendar(){
             angular.forEach($scope.appointments, function (appointment) {
                 $scope.events.push({
                     "id":appointment.id,
@@ -145,5 +51,47 @@ angular.module('appointmentsApp.controllers', []).controller('mainController',
 
                 })
             });
+        }
+
+        /**
+         *
+         *      FHIR SERVICE OAUTH2 AUTHENTICATION & APP INITIALIZATION
+         *
+         **/
+        $fhirApiServices.initClient().then(function(){
+            if ($fhirApiServices.hasPatientContext()) {
+                $fhirApiServices.queryPatient()
+                    .done(function(patientResult){
+                        $scope.patient = patientResult;
+                        var query = {patient: $scope.patient.id};
+                        $scope.user = $scope.patient;
+                        $fhirApiServices.queryResourceInstances("Appointment", query)
+                            .done(function(resourceResults){
+                                angular.forEach(resourceResults, function (resource) {
+                                    $scope.appointments.push(resource);
+
+                                });
+                                buildCalendar();
+                                $rootScope.$digest();
+                            });
+                    }).fail(function(){
+                    });
+            } else {
+                $fhirApiServices.getFhirProfileUser()
+                    .done(function(profileResult){
+                        $scope.profile = profileResult;
+                        var query = {practitioner: $scope.profile.id};
+                        $scope.user = $scope.profile;
+                        $fhirApiServices.queryResourceInstances("Appointment", query)
+                            .done(function(resourceResults){
+                                angular.forEach(resourceResults, function (resource) {
+                                    $scope.appointments.push(resource);
+                                });
+                                buildCalendar();
+                                $rootScope.$digest();
+                            });
+                    });
+            }
         });
-    });
+
+    }]);
